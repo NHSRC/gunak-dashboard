8cdc275e5372c553494e8091a824df089d509283
Vivek Singh <petmongrels@gmail.com>
2020-11-12 16:23:06 +0530

    improved login style

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,46 +1,45 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <Container className="vertical-center">
-                    <Row>
-                        <br/>
-                        <br/>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <h2>Please provide your login details</h2>
-                        <br/>
-                        <br/>
-                        <br/>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <div className="form-group">
-                            <label><b>Email</b></label>
-                            <Form.Control type="email" size="lg"/>
-                        </div>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <div className="form-group">
-                            <label><b>Password</b></label>
-                            <Form.Control type="password" size="lg"/>
-                        </div>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <button type="submit" className="btn btn-dark btn-lg">Sign in</button>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <p className="forgot-password text-right">
-                            Forgot <a href="#">password?</a>
-                        </p>
-                    </Row>
-                </Container>
-
-            </form>
-        );
-    }
+export function Login() {
+    const [count, setCount] = useState(0);
+    return (
+        <Form>
+            <Container className="vertical-center">
+                <br/>
+                <br/>
+                <Row className="justify-content-md-center">
+                    <h2>Please provide your login details</h2>
+                </Row>
+                <br/>
+                <br/>
+                <Form.Row className="justify-content-md-center">
+                    <Form.Group as={Col} md="4" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control required type="text"/>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row className="justify-content-md-center">
+                    <Form.Group as={Col} md="4" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control required type="password"/>
+                    </Form.Group>
+                </Form.Row>
+                <br/>
+                <Form.Row className="justify-content-md-center">
+                    <Form.Group as={Col} md="3">
+                        <Button variant="primary" size="lg" block>Sign in</Button>
+                    </Form.Group>
+                </Form.Row>
+                <Row className="justify-content-md-center">
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </Row>
+            </Container>
+        </Form>);
 }
