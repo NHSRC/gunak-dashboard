9a982c05dd48d3b4eba9eb21cd0b41190272df85
Vivek Singh <petmongrels@gmail.com>
2020-11-12 15:51:28 +0530

    basic styling

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,32 +1,46 @@
 import React, {Component} from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
 
 export default class Login extends Component {
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <form>
-                        <h3>Log in</h3>
-
+            <form>
+                <Container className="vertical-center">
+                    <Row>
+                        <br/>
+                        <br/>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <h2>Please provide your login details</h2>
+                        <br/>
+                        <br/>
+                        <br/>
+                    </Row>
+                    <Row className="justify-content-md-center">
                         <div className="form-group">
-                            <label>Email</label>
-                            <input type="email" className="form-control" placeholder="Enter email"/>
+                            <label><b>Email</b></label>
+                            <Form.Control type="email" size="lg"/>
                         </div>
-
+                    </Row>
+                    <Row className="justify-content-md-center">
                         <div className="form-group">
-                            <label>Password</label>
-                            <input type="password" className="form-control" placeholder="Enter password"/>
+                            <label><b>Password</b></label>
+                            <Form.Control type="password" size="lg"/>
                         </div>
-
-                        <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <button type="submit" className="btn btn-dark btn-lg">Sign in</button>
+                    </Row>
+                    <Row className="justify-content-md-center">
                         <p className="forgot-password text-right">
                             Forgot <a href="#">password?</a>
                         </p>
-                    </form>
-                </Row>
-            </Container>
+                    </Row>
+                </Container>
+
+            </form>
         );
     }
 }
